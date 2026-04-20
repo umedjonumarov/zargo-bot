c8baa452f811a1d09255f4a8755bf3561a731e98
umedjonumarov <umedjonumarov9420@gmail.com>
2026-04-20 21:33:31 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+from aiogram.types import WebAppInfo, InlineKeyboardMarkup, InlineKeyboardButton
+
+# Ўзингнинг маълумотларингни шу ерга ёз:
+TOKEN = "8635095454:AAHXJ080T4_dgmfUCeVxXMDOGOJ4MtcfdUA"  # BotFather берган токен
+ADMIN_ID = 888856976          # Сенинг Telegram ID рақаминг
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: types.Message):
+    # Диққат: index.html файлинг қаердалигини кўрсатамиз
+    # Ҳозирча тест учун оддий тугма қиламиз
+    markup = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Do'konni ochish 🛍", web_app=WebAppInfo(url="https://google.com"))] 
+    ])
+    await message.answer("ZarGo дўконига хуш келибсиз!", reply_markup=markup)
+
+async def main():
+    print("Бот ишга тушди...")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
