81812afcd2302063e3d65991baa2190de2ed5d30
umedjonumarov <umedjonumarov9420@gmail.com>
2026-04-20 22:32:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 from aiogram.types import WebAppInfo, InlineKeyboardMarkup, InlineKeyboardButton
 
-# Ўзингнинг маълумотларингни шу ерга ёз:
-TOKEN = "8635095454:AAHXJ080T4_dgmfUCeVxXMDOGOJ4MtcfdUA"  # BotFather берган токен
-ADMIN_ID = 888856976          # Сенинг Telegram ID рақаминг
+# Токенни текшириб олинг, @BotFather бергани бўлиши керак
+TOKEN = "8635095454:AAHXJ080T4_dgmfUCeVxXMDOGOJ4MtcfdUA"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def start(message: types.Message):
-    # Диққат: index.html файлинг қаердалигини кўрсатамиз
-    # Ҳозирча тест учун оддий тугма қиламиз
+    # Бу ерга Render-даги кўк лингингизни қўйинг
+    web_url = "https://zargo-bot.onrender.com" 
+    
     markup = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="Do'konni ochish 🛍", web_app=WebAppInfo(url="https://zargo-bot.onrender.com"))] 
+        [InlineKeyboardButton(text="Do'konni ochish 🛍", web_app=WebAppInfo(url=web_url))]
     ])
     await message.answer("ZarGo дўконига хуш келибсиз!", reply_markup=markup)
 
